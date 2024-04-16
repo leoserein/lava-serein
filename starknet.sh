a5719486cd8af819e35440746ad266fbd33212ce
leoserein <ranggathea.rt@gmail.com>
2024-04-16 11:23:25 +0700

    starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-a62404eb-fa21-4f94-98aa-9ef4884668e6/"
 
 OUTPUT_DIR="logs"
 
